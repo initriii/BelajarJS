822e0e0b5d559292102720c0aa2b22b4fb09a753
initriii <dewintatri13@upi.edu>
2023-10-17 14:39:20 +0700

    add file

---
+++ main.js
@@ -0,0 +1,51 @@
+// console.log("Hello World!")
+// alert("Notifikasi")
+// prompt("Pinjam seratus")
+
+// var  Promnet = "Coding is easy"
+// console.log(Promnet)
+// var Promnet = "Coding not easy"
+// console.log(Promnet)
+
+// let  Promnet = "Coding is easy"
+// console.log(Promnet)
+// Promnet = "Coding not easy"
+// console.log(Promnet)
+
+// const  Promnet = "Coding is easy"
+// console.log(Promnet)
+// Promnet = "Coding not easy"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Masukan poin anda")
+//     if(totalPoin > 100) {
+//         document.write("<h1>Congratulation</h1>");
+//     }
+//     else {
+//         document.write("<h1>Thank You</h1>");
+//     }
+
+// let x = 6;
+// let y = 3;
+
+// console.log(x<10 && y>1);
+// console.log(x<10 && y<1);
+// console.log(x==5 || y==5);
+// console.log(x==6 || y==5);
+// console.log(!(x==y));
+
+// document.writeln(x<10 && y>1);
+// document.writeln(x<10 && y<1);
+// document.writeln(x==5 || y==5);
+// document.writeln(x==6 || y==5);
+// document.writeln(!(x==y));
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+
+// button.addEventListener('click', function() {
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
