d65e2c1c1e9c6de53fdf60fd77538deb74fd551a
initriii <dewintatri13@upi.edu>
2023-10-18 17:27:55 +0700

    update main.js

--- main.js
+++ main.js
@@ -49,3 +49,99 @@
 //     console.log(isi)
 //     p.innerHTML = isi
 // })
+
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#task");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+let taskCount = 0;
+
+const displayCount = () => {
+    countValue.innerText = taskCount;
+};
+
+const updateTaskCount = (change) => {
+    taskCount += change;
+    displayCount();
+};
+
+const addTask = () => {
+    const taskName = newTaskInput.value.trim();
+    error.style.display = "none";
+    if (!taskName) {
+        setTimeout(() => {
+            error.style.display = "block";
+        }, 200);
+        return;
+    }
+
+    const task = `<div class="task">
+        <input type="checkbox" class="task-check">
+        <span class="taskname">${taskName}</span>
+        <button class="edit">
+            <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button class="delete">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    </div>`;
+
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach((button) => {
+        button.onclick = () => {
+            const taskContainer = button.closest('.task');
+            const checkBox = taskContainer.querySelector('.task-check');
+
+            if (checkBox.checked) {
+                // Jika checkbox dicentang, tidak kurangi taskCount
+            } else {
+                updateTaskCount(-1); // Kurangi hanya jika checkbox tidak dicentang
+            }
+
+            taskContainer.remove();
+        };
+    });
+
+    const editButtons = document.querySelectorAll(".edit");
+    editButtons.forEach((editBtn) => {
+        editBtn.onclick = (e) => {
+            let targetElement = e.target;
+            if (!(e.target.className == "edit")) {
+                targetElement = e.target.parentElement;
+            }
+            newTaskInput.value = targetElement.previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            updateTaskCount(-1);
+        };
+    });
+
+    const tasksCheck = document.querySelectorAll(".task-check");
+    tasksCheck.forEach((checkBox) => {
+        checkBox.onchange = () => {
+            const taskContainer = checkBox.closest('.task');
+            const taskName = taskContainer.querySelector('.taskname');
+
+            if (checkBox.checked) {
+                taskName.classList.add("completed");
+                updateTaskCount(-1); // Kurangi hanya jika checkbox belum dicentang sebelumnya
+            } else {
+                taskName.classList.remove("completed");
+            }
+        };
+    });
+
+    taskCount += 1;
+    displayCount();
+    newTaskInput.value = "";
+};
+
+addBtn.addEventListener("click", addTask);
+
+window.onload = () => {
+    taskCount = 0;
+    displayCount();
+    newTaskInput.value = "";
+};
